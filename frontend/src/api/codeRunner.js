828f158a9058f7778a78322d3af15e43039d8468
Anees Sohail <work.aneessohail@gmail.com>
2026-01-21 13:15:36 +0500

    Added Judge0 request configs (submit & poll) in api/ folder

---
+++ frontend/src/api/codeRunner.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://ce.judge0.com/submissions';
+
+// Request config for submitting code
+export const getSubmitConfig = (code, languageId) => ({
+  url: `${BASE_URL}/?base64_encoded=false&wait=false`,
+  method: 'POST',
+  body: {
+    source_code: code,
+    language_id: languageId,
+    stdin: '',
+  },
+});
+
+// Request config for polling submission status
+export const getPollConfig = (token) => ({
+  url: `${BASE_URL}/${token}?base64_encoded=false`,
+  method: 'GET',
+});
